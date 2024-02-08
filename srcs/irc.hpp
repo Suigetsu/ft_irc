3d77c7d7d56d7156d3460152e0cd573eb5467a5d
Suigetsu <lagmed.019@gmail.com>
2024-02-08 14:04:20 +0100

    added the header files needed for now

--- srcs/irc.hpp
+++ srcs/irc.hpp
@@ -6,10 +6,22 @@
 /*   By: mlagrini <mlagrini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 10:43:38 by mlagrini          #+#    #+#             */
-/*   Updated: 2024/02/03 12:15:26 by mlagrini         ###   ########.fr       */
+/*   Updated: 2024/02/08 14:03:35 by mlagrini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#ifndef IRC_HPP
+# define IRC_HPP
 
-#include <iostream>
+# include <iostream>
+# include <sys/socket.h>
+# include <unistd.h>
+# include <netdb.h>
+# include <sys/types.h>
+# include <arpa/inet.h>
+# include <netinet/in.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+#endif
